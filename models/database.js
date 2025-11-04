740f4e1eeb447e34268d578143d8219e2c87b2cd
gotayjosue <gotayjosue@gmail.com>
2025-11-03 23:10:48 -0600

    Connection to DB improved

--- models/database.js
+++ models/database.js
@@ -1,11 +1,22 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
 
+let isConnected = false; // 👈 variable global de conexión
+
 async function connectToDatabase() {
+  if (isConnected) {
+    console.log("⚡ Using existing MongoDB connection");
+    return;
+  }
+
   try {
-    await mongoose.connect(process.env.MONGODB_URI, {
+    const db = await mongoose.connect(process.env.MONGODB_URI, {
       dbName: process.env.DB_NAME || "MediLink",
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
     });
+
+    isConnected = db.connections[0].readyState === 1;
     console.log("✅ MongoDB connected with Mongoose");
   } catch (err) {
     console.error("❌ MongoDB connection error:", err.message);
