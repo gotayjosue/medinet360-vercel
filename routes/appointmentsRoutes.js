b9ba987b0e25edcfca975aa3df942b1121a75e6f
gotayjosue <gotayjosue@gmail.com>
2025-11-16 01:15:53 -0600

    appointment's route updated

--- routes/appointmentsRoutes.js
+++ routes/appointmentsRoutes.js
@@ -9,11 +9,9 @@ const { requireAuth } = require("../middleware/requireAuth.js");
 const validate = require("../middleware/appointmentValidate.js");
 const router = express.Router();
 
-router.use(requireAuth);
-
-router.get("/", getAppointments);
-router.post("/", validate.appointmentValidationRules(), validate.check, createAppointment);
-router.put("/:id", validate.appointmentValidationRules(), validate.check, updateAppointment);
-router.delete("/:id", deleteAppointment);
+router.get("/", requireAuth, getAppointments);
+router.post("/", requireAuth, validate.appointmentValidationRules(), validate.check, createAppointment);
+router.put("/:id", requireAuth, validate.appointmentValidationRules(), validate.check, updateAppointment);
+router.delete("/:id", requireAuth, deleteAppointment);
 
 module.exports = router;
